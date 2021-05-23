c4a370326bba100c79d50004e3681ff381708731
Davide042003 <davidedeblasio2003@gmail.com>
2021-05-23 22:54:54 +0200

    update

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -19,8 +19,7 @@ class _LoginWidgetState extends State<LoginWidget> {
   String _password = '';
 
   String _validatePassword(String value) {
-    Pattern pattern =
-        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
+    Pattern pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
     RegExp regex = new RegExp(pattern);
     print(value);
     if (value.isEmpty) {
@@ -66,12 +65,49 @@ class _LoginWidgetState extends State<LoginWidget> {
             SizedBox(height: SizeConfig.screenHeight * 0.05),
             InputFieldStandard("Username/Email", 0, Icons.mail_outline),
             SizedBox(height: SizeConfig.screenHeight * 0.03),
-            InputFieldPassword(this._showPassword, () => setState(() => this._showPassword = !this._showPassword), (value) => _validatePassword(value), (value) => _password = value)
+            InputFieldPassword(
+                this._showPassword,
+                () => setState(() => this._showPassword = !this._showPassword),
+                (value) => _validatePassword(value),
+                (value) => _password = value),
+            SizedBox(height: SizeConfig.screenHeight * 0.05),
+            Container(
+                width: SizeConfig.screenWidth,
+                height: SizeConfig.screenHeight * 0.065,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromRGBO(116, 142, 243, 1),
+                        Color.fromRGBO(36, 65, 187, 1)
+                      ]),
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                              fontFamily: Constants.POPPINS,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,),
+                        ),
+                        Icon(Icons.keyboard_arrow_right_outlined, color: Colors.white,)
+                      ],
+                    ),
+                  ),
+                ))
           ],
         ),
       ),
     );
   }
 }
-
-
