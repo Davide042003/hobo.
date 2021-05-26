91569d18d9ed13328461069e5ee4f4e51e43bb62
Davide042003 <davidedeblasio2003@gmail.com>
2021-05-26 11:03:56 +0200

    update

--- lib/widgets/login_register/login_widget.dart
+++ lib/widgets/login_register/login_widget.dart
@@ -81,151 +81,157 @@ class _LoginWidgetState extends State<LoginWidget> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     final themeChange = Provider.of<DarkThemeProvider>(context);
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
 
     return Form(
       key: _formKey,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.1),
-        child: Column(
-          children: [
-            SizedBox(height: SizeConfig.screenHeight * 0.07),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Welcome",
-                  style: TextStyle(
-                      fontFamily: Constants.POPPINS,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                  color: Styles.whiteblack(themeChange.darkTheme, context)),
-                  textAlign: TextAlign.left,
-                )),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Enter your login credentials",
-                    style: TextStyle(
-                        fontFamily: Constants.POPPINS,
-                        fontWeight: FontWeight.w300,
-                        fontSize: 17,
-                        color: Styles.loginregister_subheadingandform(themeChange.darkTheme, context)),
-                    textAlign: TextAlign.left)),
-            SizedBox(height: SizeConfig.screenHeight * 0.05),
-            InputFieldStandard("Username/Email", 0, Ionicons.mail_outline, (value) => _userEmail = value, false, focusNodeMail, focusNodePassword),
-            SizedBox(height: SizeConfig.screenHeight * 0.03),
-            InputFieldPassword(
-                this._showPassword,
-                () => setState(() => this._showPassword = !this._showPassword),
-                (value) => _password = value, false, focusNodePassword, focusNodeSubmit),
-            SizedBox(height: SizeConfig.screenHeight * 0.05),
-            Container(
-                width: SizeConfig.screenWidth,
-                height: SizeConfig.screenHeight * 0.065,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(35)),
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color.fromRGBO(116, 142, 243, 1),
-                        Color.fromRGBO(36, 65, 187, 1)
-                      ]),
-                ),
-                child: TextButton(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Login",
-                          style: TextStyle(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: bottom),
+        child: SingleChildScrollView(
+            child: Container(
+            margin: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.1),
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.07),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Welcome",
+                      style: TextStyle(
+                          fontFamily: Constants.POPPINS,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                      color: Styles.whiteblack(themeChange.darkTheme, context)),
+                      textAlign: TextAlign.left,
+                    )),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Enter your login credentials",
+                        style: TextStyle(
+                            fontFamily: Constants.POPPINS,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 17,
+                            color: Styles.loginregister_subheadingandform(themeChange.darkTheme, context)),
+                        textAlign: TextAlign.left)),
+                SizedBox(height: SizeConfig.screenHeight * 0.05),
+                InputFieldStandard("Username/Email", 0, Ionicons.mail_outline, (value) => _userEmail = value, false, focusNodeMail, focusNodePassword),
+                SizedBox(height: SizeConfig.screenHeight * 0.03),
+                InputFieldPassword(
+                    this._showPassword,
+                    () => setState(() => this._showPassword = !this._showPassword),
+                    (value) => _password = value, false, focusNodePassword, focusNodeSubmit),
+                SizedBox(height: SizeConfig.screenHeight * 0.05),
+                Container(
+                    width: SizeConfig.screenWidth,
+                    height: SizeConfig.screenHeight * 0.065,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(35)),
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color.fromRGBO(116, 142, 243, 1),
+                            Color.fromRGBO(36, 65, 187, 1)
+                          ]),
+                    ),
+                    child: TextButton(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                fontFamily: Constants.POPPINS,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_right_outlined,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      ),
+                    )),
+                SizedBox(height: SizeConfig.screenHeight * 0.008),
+                GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onTap: () {},
+                  child: Container(
+                    width: SizeConfig.screenWidth * 0.5,
+                    height: SizeConfig.screenHeight * 0.05,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                            color: Styles.loginregister_forgot(themeChange.darkTheme, context),
                             fontFamily: Constants.POPPINS,
                             fontSize: 17,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.06),
+                Container(
+                  width: SizeConfig.screenWidth * 0.5,
+                  height: SizeConfig.screenHeight * 0.03,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Or login with",
+                      style: TextStyle(
+                          color: Styles.loginregister_or(themeChange.darkTheme, context),
+                          fontFamily: Constants.POPPINS,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.02),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        height: SizeConfig.screenHeight * 0.11,
+                        width: SizeConfig.screenWidth * 0.18,
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: Styles.loginregister_google(themeChange.darkTheme, context),
                         ),
-                        Icon(
-                          Icons.keyboard_arrow_right_outlined,
-                          color: Colors.white,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Icon(Ionicons.logo_google, size: 30,),
+                        )),
+                    SizedBox(width: SizeConfig.screenWidth * 0.05),
+                    Container(
+                      height: SizeConfig.screenHeight * 0.11,
+                        width: SizeConfig.screenWidth * 0.18,
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: Styles.loginregister_fb(themeChange.darkTheme, context),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Icon(LineIcons.facebookF, size: 30, color: Color.fromRGBO(25, 119, 243, 1),),
+                        )),
+                    SizedBox(width: SizeConfig.screenWidth * 0.05),
+                    Container(
+                        height: SizeConfig.screenHeight * 0.11,
+                        width: SizeConfig.screenWidth * 0.18,
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: Styles.whiteblack(themeChange.darkTheme, context)),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Icon(Ionicons.logo_apple, color: Styles.blackwhite(themeChange.darkTheme, context), size: 30),
                         )
-                      ],
                     ),
-                  ),
-                )),
-            SizedBox(height: SizeConfig.screenHeight * 0.008),
-            GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onTap: () {},
-              child: Container(
-                width: SizeConfig.screenWidth * 0.5,
-                height: SizeConfig.screenHeight * 0.05,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                        color: Styles.loginregister_forgot(themeChange.darkTheme, context),
-                        fontFamily: Constants.POPPINS,
-                        fontSize: 17,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: SizeConfig.screenHeight * 0.06),
-            Container(
-              width: SizeConfig.screenWidth * 0.5,
-              height: SizeConfig.screenHeight * 0.03,
-              child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "Or login with",
-                  style: TextStyle(
-                      color: Styles.loginregister_or(themeChange.darkTheme, context),
-                      fontFamily: Constants.POPPINS,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-            ),
-            SizedBox(height: SizeConfig.screenHeight * 0.02),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    height: SizeConfig.screenHeight * 0.11,
-                    width: SizeConfig.screenWidth * 0.18,
-                    decoration: BoxDecoration(shape: BoxShape.circle, color: Styles.loginregister_google(themeChange.darkTheme, context),
-                    ),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Icon(Ionicons.logo_google, size: 30,),
-                    )),
-                SizedBox(width: SizeConfig.screenWidth * 0.05),
-                Container(
-                  height: SizeConfig.screenHeight * 0.11,
-                    width: SizeConfig.screenWidth * 0.18,
-                    decoration: BoxDecoration(shape: BoxShape.circle, color: Styles.loginregister_fb(themeChange.darkTheme, context),
-                    ),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Icon(LineIcons.facebookF, size: 30, color: Color.fromRGBO(25, 119, 243, 1),),
-                    )),
-                SizedBox(width: SizeConfig.screenWidth * 0.05),
-                Container(
-                    height: SizeConfig.screenHeight * 0.11,
-                    width: SizeConfig.screenWidth * 0.18,
-                    decoration: BoxDecoration(shape: BoxShape.circle, color: Styles.whiteblack(themeChange.darkTheme, context)),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Icon(Ionicons.logo_apple, color: Styles.blackwhite(themeChange.darkTheme, context), size: 30),
-                    )
-                ),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
--- lib/widgets/login_register/register_widget.dart
+++ lib/widgets/login_register/register_widget.dart
@@ -120,6 +120,7 @@ class _RegisterWidgetState extends State<RegisterWidget> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     final themeChange = Provider.of<DarkThemeProvider>(context);
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
 
     TextStyle defaultStyle = TextStyle(fontSize: 15, fontFamily: Constants.POPPINS, color: Styles.whiteblack(themeChange.darkTheme, context));
     TextStyle linkStyle = TextStyle(fontSize: 15, fontFamily: Constants.POPPINS, fontWeight: FontWeight.w600, color: Styles.loginregister_forgot(themeChange.darkTheme, context));
@@ -127,117 +128,122 @@ class _RegisterWidgetState extends State<RegisterWidget> {
 
     return Form(
       key: _formKey,
-      child: Container(
-          margin:
-              EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.1),
-          child: Column(children: [
-            SizedBox(height: SizeConfig.screenHeight * 0.035),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Register",
-                  style: TextStyle(
-                      fontFamily: Constants.POPPINS,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                  color: Styles.whiteblack(themeChange.darkTheme, context)),
-                  textAlign: TextAlign.left,
-                )),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Enter the following details",
-                    style: TextStyle(
-                        fontFamily: Constants.POPPINS,
-                        fontWeight: FontWeight.w300,
-                        fontSize: 17,
-                        color: Styles.loginregister_subheadingandform(themeChange.darkTheme, context)),
-                    textAlign: TextAlign.left)),
-            SizedBox(height: SizeConfig.screenHeight * 0.035),
-            InputFieldStandard("Name", 0, Ionicons.person_outline,
-                (value) => _name = value, true, focusNodeName, focusNodeUsername),
-            SizedBox(height: SizeConfig.screenHeight * 0.035),
-            InputFieldStandard("Username", 0, Ionicons.person_outline,
-                (value) => _username = value, true, focusNodeUsername, focusNodeMail),
-            SizedBox(height: SizeConfig.screenHeight * 0.035),
-            InputFieldStandard("Email Address", 1, Ionicons.mail_outline,
-                (value) => _email = value, true, focusNodeMail, focusNodePassword),
-            SizedBox(height: SizeConfig.screenHeight * 0.035),
-            InputFieldPassword(
-                this._showPassword,
-                () => setState(() => this._showPassword = !this._showPassword),
-                (value) => _password = value,
-                true, focusNodePassword, focusNodeSubmit),
-            SizedBox(
-              height: SizeConfig.screenWidth * 0.07,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  style: defaultStyle,
-                  children: <TextSpan>[
-                    TextSpan(text: 'By registering, you are agreeing to our '),
-                    TextSpan(
-                        text: 'Terms & conditions',
-                        style: linkStyle,
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            print('Terms of Service"');
-                          }),
-                    TextSpan(text: ' and '),
-                    TextSpan(
-                        text: 'Privacy \n policy.',
-                        style: linkStyle,
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            print('Privacy policy"');
-                          }),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: SizeConfig.screenHeight * 0.035),
-            Container(
-                width: SizeConfig.screenWidth,
-                height: SizeConfig.screenHeight * 0.065,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(35)),
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color.fromRGBO(116, 142, 243, 1),
-                        Color.fromRGBO(36, 65, 187, 1)
-                      ]),
-                ),
-                child: TextButton(
-                  focusNode: focusNodeSubmit,
-                  onPressed: _trySubmitForm,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Register",
-                          style: TextStyle(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: bottom),
+        child: SingleChildScrollView(
+          child: Container(
+              margin:
+                  EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.1),
+              child: Column(children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.035),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                          fontFamily: Constants.POPPINS,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                      color: Styles.whiteblack(themeChange.darkTheme, context)),
+                      textAlign: TextAlign.left,
+                    )),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Enter the following details",
+                        style: TextStyle(
                             fontFamily: Constants.POPPINS,
+                            fontWeight: FontWeight.w300,
                             fontSize: 17,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Icon(
-                          Icons.keyboard_arrow_right_outlined,
-                          color: Colors.white,
-                        )
+                            color: Styles.loginregister_subheadingandform(themeChange.darkTheme, context)),
+                        textAlign: TextAlign.left)),
+                SizedBox(height: SizeConfig.screenHeight * 0.035),
+                InputFieldStandard("Name", 0, Ionicons.person_outline,
+                    (value) => _name = value, true, focusNodeName, focusNodeUsername),
+                SizedBox(height: SizeConfig.screenHeight * 0.035),
+                InputFieldStandard("Username", 0, Ionicons.person_outline,
+                    (value) => _username = value, true, focusNodeUsername, focusNodeMail),
+                SizedBox(height: SizeConfig.screenHeight * 0.035),
+                InputFieldStandard("Email Address", 1, Ionicons.mail_outline,
+                    (value) => _email = value, true, focusNodeMail, focusNodePassword),
+                SizedBox(height: SizeConfig.screenHeight * 0.035),
+                InputFieldPassword(
+                    this._showPassword,
+                    () => setState(() => this._showPassword = !this._showPassword),
+                    (value) => _password = value,
+                    true, focusNodePassword, focusNodeSubmit),
+                SizedBox(
+                  height: SizeConfig.screenWidth * 0.07,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      style: defaultStyle,
+                      children: <TextSpan>[
+                        TextSpan(text: 'By registering, you are agreeing to our '),
+                        TextSpan(
+                            text: 'Terms & conditions',
+                            style: linkStyle,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print('Terms of Service"');
+                              }),
+                        TextSpan(text: ' and '),
+                        TextSpan(
+                            text: 'Privacy \n policy.',
+                            style: linkStyle,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print('Privacy policy"');
+                              }),
                       ],
                     ),
                   ),
-                )),
-          ])),
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.035),
+                Container(
+                    width: SizeConfig.screenWidth,
+                    height: SizeConfig.screenHeight * 0.065,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(35)),
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color.fromRGBO(116, 142, 243, 1),
+                            Color.fromRGBO(36, 65, 187, 1)
+                          ]),
+                    ),
+                    child: TextButton(
+                      focusNode: focusNodeSubmit,
+                      onPressed: _trySubmitForm,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Register",
+                              style: TextStyle(
+                                fontFamily: Constants.POPPINS,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_right_outlined,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      ),
+                    )),
+              ])),
+        ),
+      ),
     );
   }
 }
