7a894735b516935203244898bb026f01c79bc8ca
Davide042003 <davidedeblasio2003@gmail.com>
2021-06-05 13:53:26 +0200

    update

--- lib/views/paymentdetails_view.dart
+++ lib/views/paymentdetails_view.dart
@@ -20,9 +20,13 @@ class _PaymentDetailsViewState extends State<PaymentDetailsView>
   void initState() {
     super.initState();
 
-    cardType.add(new RadioModel(true, LineIcons.visaCreditCard));
-    cardType.add(new RadioModel(false, LineIcons.mastercardCreditCard));
-    cardType.add(new RadioModel(false, LineIcons.americanExpressCreditCard));
+    for (int i = 0; i < cardPaymentArrayList.length; i++) {
+      if (i == 0) {
+        cardType.add(new RadioModel(true));
+      } else {
+        cardType.add(new RadioModel(false));
+      }
+    }
 
     _controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 200));
@@ -118,45 +122,46 @@ class _PaymentDetailsViewState extends State<PaymentDetailsView>
 
   Container TopBar(DarkThemeProvider themeChange, BuildContext context) {
     return Container(
-              width: SizeConfig.screenWidth,
-              height: SizeConfig.screenHeight * 0.13,
-              child: Padding(
-                padding: EdgeInsets.only(
-                    left: SizeConfig.screenWidth * 0.02,
-                    top: SizeConfig.screenHeight * 0.04),
-                child: Row(children: [
-                  GestureDetector(
-                    child: Container(
-                      width: SizeConfig.screenWidth * 0.125,
-                      height: SizeConfig.screenHeight,
-                      child: Icon(
-                        Icons.arrow_back_ios_outlined,
-                        color:
-                            Styles.whiteblack(themeChange.darkTheme, context),
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: SizeConfig.screenWidth * 0.04),
-                  Text(
-                    "Payment Details",
-                    style: TextStyle(
-                        fontFamily: Constants.POPPINS,
-                        color:
-                            Styles.whiteblack(themeChange.darkTheme, context),
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ]),
-              ));
+        width: SizeConfig.screenWidth,
+        height: SizeConfig.screenHeight * 0.13,
+        child: Padding(
+          padding: EdgeInsets.only(
+              left: SizeConfig.screenWidth * 0.02,
+              top: SizeConfig.screenHeight * 0.04),
+          child: Row(children: [
+            GestureDetector(
+              child: Container(
+                width: SizeConfig.screenWidth * 0.125,
+                height: SizeConfig.screenHeight,
+                child: Icon(
+                  Icons.arrow_back_ios_outlined,
+                  color: Styles.whiteblack(themeChange.darkTheme, context),
+                  size: 20,
+                ),
+              ),
+            ),
+            SizedBox(width: SizeConfig.screenWidth * 0.04),
+            Text(
+              "Payment Details",
+              style: TextStyle(
+                  fontFamily: Constants.POPPINS,
+                  color: Styles.whiteblack(themeChange.darkTheme, context),
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600),
+            ),
+          ]),
+        ));
   }
 
-  AnimatedContainer CardPaymentOption(DarkThemeProvider themeChange, BuildContext context) {
+  AnimatedContainer CardPaymentOption(
+      DarkThemeProvider themeChange, BuildContext context) {
     return AnimatedContainer(
       duration: new Duration(milliseconds: 500),
       height: SizeConfig.screenHeight *
           (_openCard
-              ? (cardPaymentArrayList.length > 0 ? 0.35 + cardPaymentArrayList.length * 0.055 : 0.18)
+              ? (cardPaymentArrayList.length > 0
+                  ? 0.35 + cardPaymentArrayList.length * 0.055
+                  : 0.18)
               : 0.08),
       curve: Curves.easeInOut,
       child: _openCard
@@ -232,7 +237,9 @@ class _PaymentDetailsViewState extends State<PaymentDetailsView>
                         color: Styles.tourpreview_bar(
                             themeChange.darkTheme, context),
                       ),
-                      SizedBox(height: SizeConfig.screenHeight * (cardPaymentArrayList.length > 0 ? 0.02 : 0.005)),
+                      SizedBox(
+                          height: SizeConfig.screenHeight *
+                              (cardPaymentArrayList.length > 0 ? 0.02 : 0.005)),
                       cardPaymentArrayList.length > 0
                           ? Container(
                               width: SizeConfig.screenWidth * 0.72,
@@ -241,15 +248,22 @@ class _PaymentDetailsViewState extends State<PaymentDetailsView>
                             )
                           : SizedBox(),
                       SizedBox(
-                        height: SizeConfig.screenHeight * (cardPaymentArrayList.length > 0 ? 0.02 : 0.005),
+                        height: SizeConfig.screenHeight *
+                            (cardPaymentArrayList.length > 0 ? 0.02 : 0.005),
                       ),
                       ListView.separated(
                         padding: EdgeInsets.zero,
                         shrinkWrap: true,
                         scrollDirection: Axis.vertical,
                         itemCount: cardPaymentArrayList.length,
-                        itemBuilder: (ctx, i) => CardsOptions(index: i),
-                        separatorBuilder: (context, index) => SizedBox(height: SizeConfig.screenHeight * 0.015),
+                        itemBuilder: (ctx, i) => GestureDetector(child: CardsOptions(index: i, item: cardType[i],), onTap: () {
+                          setState(() {
+                            cardType.forEach((element) => element.isSelected = false);
+                            cardType[i].isSelected = true;
+                          });
+                        },),
+                        separatorBuilder: (context, index) =>
+                            SizedBox(height: SizeConfig.screenHeight * 0.015),
                       ),
                       SizedBox(height: SizeConfig.screenHeight * 0.01),
                       Container(
@@ -361,16 +375,16 @@ class _PaymentDetailsViewState extends State<PaymentDetailsView>
 
 class CardsOptions extends StatelessWidget {
   final int index;
+  final RadioModel item;
 
   CardsOptions({
-    @required this.index,
+    @required this.index, this.item
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: SizeConfig.screenWidth * 0.07),
+      padding: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.07),
       child: Row(
         children: [
           Container(
@@ -378,16 +392,20 @@ class CardsOptions extends StatelessWidget {
             height: SizeConfig.screenHeight * 0.03,
             decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                border: Border.all(
-                    color: Color.fromRGBO(236, 236, 236, 1))),
+                border: Border.all(color: Color.fromRGBO(236, 236, 236, 1))),
+            child: item.isSelected ? Container(
+              margin: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth *0.008),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromRGBO(36, 65, 187, 1)
+            )) : null,
           ),
           SizedBox(width: SizeConfig.screenWidth * 0.025),
           Icon(LineIcons.mastercardCreditCard, size: 30),
           SizedBox(width: SizeConfig.screenWidth * 0.025),
           Text(
             "Mastercard x-1235",
-            style: TextStyle(
-                fontFamily: Constants.POPPINS, fontSize: 15),
+            style: TextStyle(fontFamily: Constants.POPPINS, fontSize: 15),
           )
         ],
       ),
@@ -395,41 +413,10 @@ class CardsOptions extends StatelessWidget {
   }
 }
 
-
-class RadioItemPayOption extends StatelessWidget {
-  final RadioModel _item;
-
-  RadioItemPayOption(this._item);
-
-  @override
-  Widget build(BuildContext context) {
-    SizeConfig().init(context);
-
-    return new Container(
-      width: SizeConfig.screenWidth * 0.2,
-      height: SizeConfig.screenHeight * 0.045,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          border: Border.all(
-              color: _item.isSelected
-                  ? Color.fromRGBO(26, 65, 187, 1)
-                  : Color.fromRGBO(236, 236, 236, 1)),
-          color: Colors.white),
-      child: Center(
-        child: Icon(
-          _item.icon,
-          size: 35,
-        ),
-      ),
-    );
-  }
-}
-
 class RadioModel {
   bool isSelected;
-  final IconData icon;
 
-  RadioModel(this.isSelected, this.icon);
+  RadioModel(this.isSelected);
 }
 
 class CardItem extends StatelessWidget {
