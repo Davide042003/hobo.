23c22e90d3e8dca1abb8666166898dd22d62cb65
Davide042003 <davidedeblasio2003@gmail.com>
2021-06-07 14:57:15 +0200

    update

--- lib/widgets/chat/cardchat_widget.dart
+++ lib/widgets/chat/cardchat_widget.dart
@@ -26,28 +26,42 @@ class Bubble extends StatelessWidget {
             topLeft: Radius.circular(15.0),
             topRight: Radius.circular(15.0),
           );
-    return Column(
-      crossAxisAlignment: align,
-      children: <Widget>[
-        Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.04, vertical: SizeConfig.screenHeight * 0.012),
-              decoration: BoxDecoration(
-                color: bg,
-                borderRadius: radius,
-              ),
-              child: Text(message,
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.04),
+      decoration: BoxDecoration(
+        color: bg,
+        borderRadius: radius,
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        title: Text(message,
+            style: TextStyle(
+              fontFamily: Constants.POPPINS,
+              color: Styles.whiteblack(themeChange.darkTheme, context),
+              fontSize: 14.0,
+            )),
+        subtitle: Align(
+          alignment: Alignment.bottomRight,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(time,
                   style: TextStyle(
                     fontFamily: Constants.POPPINS,
-                    color: Styles.whiteblack(themeChange.darkTheme, context),
-                    fontSize: 14.0,
+                    color: Color.fromRGBO(183, 183, 183, 1),
+                    fontSize: 10.0,
                   )),
-              constraints: BoxConstraints(maxWidth: SizeConfig.screenWidth*0.75),
-            ),
-          ],
+              SizedBox(width: SizeConfig.screenWidth*0.007),
+              Icon(
+                icon,
+                size: 12.0,
+                color: Colors.black38,
+              )
+            ],
+          ),
         ),
-      ],
+      ),
+      constraints: BoxConstraints(maxWidth: SizeConfig.screenWidth*0.75),
     );
   }
 }
