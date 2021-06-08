fc424675e128f525e325de99b8296662c5421ea8
Davide042003 <davidedeblasio2003@gmail.com>
2021-06-08 16:14:48 +0200

    update

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -24,6 +24,7 @@ class _ProfileViewState extends State<ProfileView> with TickerProviderStateMixin
   final ScrollController _scrollController = ScrollController();
   double animationPos;
   bool visible = false;
+  bool pinnedAppBar = false;
 
   @override
   void initState() {
@@ -36,6 +37,16 @@ class _ProfileViewState extends State<ProfileView> with TickerProviderStateMixin
       } else if (_scrollController.offset <= SizeConfig.screenHeight * 0.18) {
         setState(() {
           visible = false;
+        });
+      }
+
+      if (_scrollController.offset >= SizeConfig.screenHeight * 0.448) {
+        setState(() {
+          pinnedAppBar = true;
+        });
+      } else if (_scrollController.offset < SizeConfig.screenHeight * 0.448) {
+        setState(() {
+          pinnedAppBar = false;
         });
       }
 
@@ -576,7 +587,7 @@ class _ProfileViewState extends State<ProfileView> with TickerProviderStateMixin
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: [
-                            Stack(children: [
+                          pinnedAppBar ? SizedBox(height: SizeConfig.screenHeight *0.05,) :  Stack(children: [
                               Container(
                                 margin: EdgeInsets.only(
                                     top: SizeConfig.screenHeight * 0.0475),
@@ -737,7 +748,111 @@ class _ProfileViewState extends State<ProfileView> with TickerProviderStateMixin
                 ),
               ),
             ),
-          ):SizedBox()
+          ):SizedBox(),
+          pinnedAppBar ? Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: SizeConfig.screenWidth * 0.01),
+            child: Container(
+              margin: EdgeInsets.only(top: SizeConfig.screenHeight * 0.099),
+              width: SizeConfig.screenWidth,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.only(top: SizeConfig.screenHeight * 0.01),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Stack(children: [
+                      AnimatedPositioned(
+                        left: _leftPosition * SizeConfig.screenWidth,
+                        child: Container(
+                          margin: EdgeInsets.only(
+                              top: SizeConfig.screenHeight * 0.045),
+                          decoration: BoxDecoration(
+                              color: Styles.tourpreview_colorprice(
+                                  themeChange.darkTheme, context),
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10))),
+                          width: SizeConfig.screenWidth * 0.326,
+                          height: SizeConfig.screenHeight * 0.003,
+                        ),
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                              behavior: HitTestBehavior.translucent,
+                              child: Container(
+                                width: SizeConfig.screenWidth * 0.326,
+                                height: SizeConfig.screenHeight * 0.0485,
+                                child: Center(
+                                    child: Text(
+                                      "Tours",
+                                      style: TextStyle(
+                                          fontFamily: Constants.POPPINS,
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 15,
+                                          color: _currentPage == 0
+                                              ? Styles.tourpreview_colorprice(
+                                              themeChange.darkTheme, context)
+                                              : Styles.profile_disabledcolor(
+                                              themeChange.darkTheme,
+                                              context)),
+                                    )),
+                              ),
+                              onTap: () => _pageController.jumpToPage(0)),
+                          GestureDetector(
+                              behavior: HitTestBehavior.translucent,
+                              child: Container(
+                                width: SizeConfig.screenWidth * 0.326,
+                                height: SizeConfig.screenHeight * 0.0485,
+                                child: Center(
+                                    child: Text(
+                                      "Gallery",
+                                      style: TextStyle(
+                                          fontFamily: Constants.POPPINS,
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 15,
+                                          color: _currentPage == 1
+                                              ? Styles.tourpreview_colorprice(
+                                              themeChange.darkTheme, context)
+                                              : Styles.profile_disabledcolor(
+                                              themeChange.darkTheme,
+                                              context)),
+                                    )),
+                              ),
+                              onTap: () => _pageController.jumpToPage(1)),
+                          GestureDetector(
+                              behavior: HitTestBehavior.translucent,
+                              child: Container(
+                                width: SizeConfig.screenWidth * 0.326,
+                                height: SizeConfig.screenHeight * 0.0485,
+                                child: Center(
+                                    child: Text(
+                                      "Reviews",
+                                      style: TextStyle(
+                                          fontFamily: Constants.POPPINS,
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 15,
+                                          color: _currentPage == 2
+                                              ? Styles.tourpreview_colorprice(
+                                              themeChange.darkTheme, context)
+                                              : Styles.profile_disabledcolor(
+                                              themeChange.darkTheme,
+                                              context)),
+                                    )),
+                              ),
+                              onTap: () => _pageController.jumpToPage(2)),
+                        ],
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          ) : SizedBox(),
         ],
       ),
     );
