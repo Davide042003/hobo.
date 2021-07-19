4c99003d691052a02bac287d2360eded31d720cc
Davide042003 <davidedeblasio2003@gmail.com>
2021-07-19 11:23:17 +0200

    update

--- lib/widgets/custom_icons/custom_bar_icons.dart
+++ lib/widgets/custom_icons/custom_bar_icons.dart
@@ -11,7 +11,7 @@
 ///      fonts:
 ///       - asset: fonts/CustomIcons.ttf
 ///
-/// 
+///
 ///
 import 'package:flutter/widgets.dart';
 
@@ -20,6 +20,7 @@ class CustomIcons {
 
   static const _kFontFam = 'CustomIcons';
   static const String _kFontPkg = null;
+
 
   static const IconData homeicon = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData chaticon = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
@@ -56,4 +57,8 @@ class CustomIcons {
   static const IconData guide = IconData(0xe839, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData diamond = IconData(0xe83a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData check = IconData(0xe83b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData linktour = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData addplace = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pen = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData no_under18 = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
